f1b5165d78176830f07a65520ef33866adbd433f
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-12-02 00:29:26 +0000

    Added ControllerBase

---
+++ DNI.Core.Web/ControllerBase.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using DNI.Core.Shared.Contracts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DNI.Core.Web
+{
+    public abstract class ControllerBase : Microsoft.AspNetCore.Mvc.ControllerBase
+    {
+        protected ControllerBase(IMediator mediator, IMapper mapper)
+        {
+            Mediator = mediator;
+            Mapper = mapper;
+        }
+
+        public IMediator Mediator { get; }
+
+        public IMapper Mapper { get; }
+
+        public IActionResult AttemptedResult<T>(IAttempt<T> attempt, bool useCreatedResponse = false, string key = default)
+        {
+            if (attempt.Successful)
+            {
+                if(useCreatedResponse)
+                { 
+                    return Created(GetCurrentUrl(),Request.Scheme);
+                }
+
+                return Ok(attempt.Result);
+            }
+
+            if(key == default)
+            {
+                key = string.Empty;
+            }
+
+            ModelState.AddModelError(key, attempt.Exception.Message);
+
+            return BadRequest(ModelState);
+        }
+
+        protected Uri GetCurrentUrl()
+        {
+            var request = HttpContext.Request;
+            return new Uri($"{request.Scheme}://{request.Host}"); 
+        }
+    }
+}
