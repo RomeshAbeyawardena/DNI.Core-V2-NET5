bc556745f1d9948feb12c45cadbdd15ff8ba6921
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-12-02 00:02:14 +0000

    Added ServiceRegistration helper

---
+++ DNI.Core.Abstractions/ServiceRegistrationBase.cs
@@ -0,0 +1,35 @@
+﻿using DNI.Core.Shared;
+using DNI.Core.Shared.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Abstractions
+{
+    public static class ServiceRegistration 
+    {
+        public static IServiceCollection RegisterServices<TServiceRegistration>(IServiceCollection services)
+            where TServiceRegistration : IServiceRegistration
+        {
+            var serviceRegistration = Activator.CreateInstance<TServiceRegistration>();
+            serviceRegistration.RegisterServices(services);
+            return services;
+        }
+    }
+
+    public abstract class ServiceRegistrationBase : IServiceRegistration
+    {
+        public abstract IServiceCollection RegisterServices(IServiceCollection services);
+
+        protected static IDefinition<Assembly> GetAssemblies(Action<IDefinition<Assembly>> defineAssemblies)
+        {
+            var assemblyDefinition = Definition.CreateAssemblyDefinition(defineAssemblies);
+
+            return assemblyDefinition;
+        }
+    }
+}
