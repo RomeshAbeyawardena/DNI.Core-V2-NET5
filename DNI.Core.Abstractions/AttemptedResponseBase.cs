b8f829354006695bd36e58e100937c35e187e31e
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-12-04 13:01:59 +0000

    Added constructors to instantiate abstract properties

--- DNI.Core.Abstractions/AttemptedResponseBase.cs
+++ DNI.Core.Abstractions/AttemptedResponseBase.cs
@@ -6,6 +6,29 @@ namespace DNI.Core.Abstractions
 {
     public abstract class AttemptedResponseBase<T> : IAttemptedResponse<T>
     {
+        protected AttemptedResponseBase (
+            IAttempt<T> attempt = default,
+            IAttempt<IEnumerable<T>> attemptMany = default,
+            RequestQueryType type = RequestQueryType.None)
+        {
+            Type = type;
+            Attempt = attempt;
+            AttemptMany = attemptMany;
+            Type = type;
+        }
+
+        protected AttemptedResponseBase (IAttempt<T> attempt, RequestQueryType type = RequestQueryType.Single)
+            : this(attempt, null, type)
+        {
+
+        }
+
+        protected AttemptedResponseBase (IAttempt<IEnumerable<T>> attemptMany, RequestQueryType type = RequestQueryType.Multiple)
+            : this(null, attemptMany, type)
+        {
+
+        }
+
         public RequestQueryType Type { get; }
         public IAttempt<T> Attempt { get; }
         public IAttempt<IEnumerable<T>> AttemptMany { get; }
