49e790598b32f4b6021b86404b57db8dbadb9604
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-12-14 13:24:00 +0000

    Added extension methods

---
+++ DNI.Core.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using DNI.Core.Shared.Extensions;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        [Test]
+        public void TryAddOrCreate()
+        {
+            Assert.IsNull(namesList);
+
+            Shared.Extensions.CollectionExtensions.TryAddOrCreate(ref namesList, "Tom");
+
+            Assert.IsNotNull(namesList);
+
+            Assert.Contains("Tom", namesList as ICollection);
+        }
+
+        private ICollection<string> namesList;
+    }
+}
