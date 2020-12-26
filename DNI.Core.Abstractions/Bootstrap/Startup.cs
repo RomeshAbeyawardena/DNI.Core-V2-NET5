983ceeccef25c35640b697b183c79f070c4fdd88
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-12-26 20:39:56 +0000

    Added bootstrap classes

---
+++ DNI.Core.Abstractions/Bootstrap/Startup.cs
@@ -0,0 +1,30 @@
+﻿using DNI.Core.Abstractions.Extensions;
+using DNI.Core.Shared.Contracts;
+using DNI.Core.Shared.Enumerations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Abstractions.Bootstrap
+{
+    public abstract class Startup<TServiceRegistration>
+        where TServiceRegistration : IServiceRegistration
+    {
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .RegisterServices<TServiceRegistration>()
+                .AddDistributedMemoryCache();
+        }
+
+        public abstract void Configure(IApplicationBuilder app, IWebHostEnvironment env);
+    }
+}
---
+++ DNI.Core.Data/Bootstrap/ServiceRegistration.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using DNI.Core.Abstractions;
+using DNI.Core.Data.Extensions;
+using DNI.Core.Shared.Enumerations;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Data
+{
+    public abstract class ServiceRegistration<TDbContext> : ServiceRegistrationBase
+        where TDbContext : DbContext
+    {
+        public DbContextMethod DbContextMethod { get; }
+        public ServiceLifetime ServiceLifetime { get; }
+        
+        public abstract IEnumerable<Assembly> ServiceAssemblies { get; }
+        public abstract IEnumerable<Assembly> DomainAssemblies { get; }
+
+        protected ServiceRegistration(DbContextMethod dbContextMethod = DbContextMethod.DbContextPool, 
+            ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+        {
+            DbContextMethod = dbContextMethod;
+            ServiceLifetime = serviceLifetime;
+        }
+
+        public override IServiceCollection RegisterServices(IServiceCollection services)
+        {
+            ConfigureServices(services);
+
+            return services
+                .AddMediatR(ServiceAssemblies.ToArray())
+                .AddAutoMapper(DomainAssemblies.ToArray())
+                .RegisterDbContextWithRepositories<TDbContext>(DbContextMethod, 
+                    dbContextOptionsServiceInjectionAction: Builder, 
+                    serviceLifetime: ServiceLifetime);
+        }
+
+        public abstract void ConfigureServices(IServiceCollection services);
+
+        public abstract void Builder(IServiceProvider serviceProvider, DbContextOptionsBuilder builder);
+    }
+}
