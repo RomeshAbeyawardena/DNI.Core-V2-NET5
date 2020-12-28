69a4945e45dd64280662af4182ee2e2fbefc2b73
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-12-28 02:28:51 +0000

    Added extensions

--- DNI.Core.Shared/Builders.cs
+++ DNI.Core.Shared/Builders.cs
@@ -9,24 +9,59 @@ namespace DNI.Core.Shared
 {
     public static class Builders
     {
+        /// <summary>
+        /// Creates a dictionary builder
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <returns></returns>
         public static IDictionaryBuilder<TKey, TValue> Dictionary<TKey, TValue>()
         {
-            return new DictionaryBuilder<TKey, TValue>();
+            return GetBuilder().Create<TKey, TValue>();
         }
 
+        /// <summary>
+        /// Creates a list builder
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static IListBuilder<T> List<T>()
         {
-            return new ListBuilder<T>();
+            return GetBuilder().Create<T>();
         }
 
+        /// <summary>
+        /// Creates a dictionary builder with a declared build action
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="buildAction"></param>
+        /// <returns></returns>
         public static IDictionaryBuilder<TKey, TValue> Dictionary<TKey, TValue>(Action<IDictionaryBuilder<TKey, TValue>> buildAction)
         {
-            return new DictionaryBuilder<TKey, TValue>(buildAction);
+            return GetBuilder().Create(buildAction);
         }
 
+        /// <summary>
+        /// Creates a list builder with a declared build action
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="buildAction"></param>
+        /// <returns></returns>
         public static IListBuilder<T> List<T>(Action<IListBuilder<T>> buildAction)
         {
-            return new ListBuilder<T>(buildAction);
+            return GetBuilder().Create(buildAction);
         }
+
+        /// <summary>
+        /// Gets or creates a builder to create lists or dictionary builders
+        /// </summary>
+        /// <returns></returns>
+        public static IBuilder GetBuilder()
+        {
+            return builder ??= new Builder();
+        }
+
+        private static IBuilder builder;
     }
 }
