2ed722398a9c49104ed8e3668145a84400152cb3
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-12-31 14:56:17 +0000

    Added async methods

--- DNI.Core.Shared/Contracts/IIncludeableQuery.cs
+++ DNI.Core.Shared/Contracts/IIncludeableQuery.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DNI.Core.Shared.Contracts
 {
@@ -9,5 +11,42 @@ namespace DNI.Core.Shared.Contracts
         where T : class
     {
         IIncludeableQuery<T> Includes<TSource>(Expression<Func<T, TSource>> includeExpression);
+
+        Task<T> FirstOrDefaultAsync(
+            Expression<Func<T, bool>> whereExpression = default,
+            CancellationToken? cancellationToken = null);
+
+        Task<T> SingleOrDefaultAsync(
+            Expression<Func<T, bool>> whereExpression = default,
+            CancellationToken? cancellationToken = null);
+
+        Task<IEnumerable<T>> ToArrayAsync(
+            Expression<Func<T, bool>> whereExpression = default,
+            CancellationToken? cancellationToken = null);
+
+        Task<bool> AnyAsync(
+            Expression<Func<T, bool>> whereExpression = default,
+            CancellationToken? cancellationToken = null);
+
+        Task<int> CountAsync(
+            Expression<Func<T, bool>> whereExpression = default,
+            CancellationToken? cancellationToken = null);
+
+        
+        Task<T> FirstOrDefaultAsync(
+            CancellationToken? cancellationToken = null);
+
+        Task<T> SingleOrDefaultAsync(
+            CancellationToken? cancellationToken = null);
+
+        Task<IEnumerable<T>> ToArrayAsync(
+            CancellationToken? cancellationToken = null);
+
+        Task<bool> AnyAsync(
+            CancellationToken? cancellationToken = null);
+
+        Task<int> CountAsync(
+            CancellationToken? cancellationToken = null);
+
     }
 }
--- DNI.Core.Shared/Contracts/Services/IModelEncryptionService.cs
+++ DNI.Core.Shared/Contracts/Services/IModelEncryptionService.cs
@@ -1,8 +1,21 @@
 ﻿namespace DNI.Core.Shared.Contracts.Services
 {
+    /// <summary>
+    /// Represents a model encryption service to encrypt and decrypt a model
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IModelEncryptionService<T> 
     {
+        /// <summary>
+        /// Encrypts a model
+        /// </summary>
+        /// <param name="model"></param>
         void Encrypt(T model);
+
+        /// <summary>
+        /// Decrypts a model
+        /// </summary>
+        /// <param name="model"></param>
         void Decrypt(T model);
     }
 }
