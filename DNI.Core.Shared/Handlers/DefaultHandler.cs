bdf35afa8dc500d8c98f87f65c05f552334f3929
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2020-12-31 11:46:18 +0000

    Re-used base properties

--- DNI.Core.Shared/Handlers/DefaultHandler.cs
+++ DNI.Core.Shared/Handlers/DefaultHandler.cs
@@ -8,16 +8,14 @@ namespace DNI.Core.Shared.Handlers
     {
         public ITryHandler Try(Action action, Action<ICatchHandler> catchAction, Action<IFinallyHandler> finalAction)
         {
-            return (CatchHandler != null ||
-                FinallyHandler != null) 
+            return IsCatchOrFinallyHandlerNull 
                 ? DefaultTryHandler.Create(CatchHandler, FinallyHandler, action, catchAction, finalAction) 
                 : DefaultTryHandler.Create(action, catchAction, finalAction);
         }
 
         public ITryHandler<TResult> Try<TResult>(Func<TResult> resultAction, Action<ICatchHandler> catchAction, Action<IFinallyHandler> finalAction)
         {
-            return (CatchHandler != null ||
-                FinallyHandler != null) 
+            return IsCatchOrFinallyHandlerNull
                 ? DefaultTryHandler<TResult>.Create(CatchHandler, FinallyHandler, resultAction, catchAction, finalAction) 
                 : DefaultTryHandler<TResult>.Create(resultAction, catchAction, finalAction);
         }
@@ -35,5 +33,8 @@ namespace DNI.Core.Shared.Handlers
 
         protected ICatchHandler CatchHandler { get; set; }
         protected IFinallyHandler FinallyHandler { get; set; }
+
+        private bool IsCatchOrFinallyHandlerNull => CatchHandler != null ||
+                FinallyHandler != null;
     }
 }
