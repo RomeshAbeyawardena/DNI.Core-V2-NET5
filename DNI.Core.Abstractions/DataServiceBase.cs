68acfa902fd1f49cfc3513d1a8b3ec5d0b7da0b3
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-07 19:09:05 +0000

    Added DataServiceBase

---
+++ DNI.Core.Abstractions/DataServiceBase.cs
@@ -0,0 +1,27 @@
+﻿using DNI.Core.Shared.Contracts;
+using DNI.Core.Shared.Contracts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Abstractions
+{
+    public abstract class DataServiceBase<TEntity> : IDataService<TEntity>
+        where TEntity : class
+    {
+        public Task<int> SaveChanges(CancellationToken cancellationToken)
+        {
+            return Repository.SaveChangesAsync(cancellationToken);
+        }
+
+        protected DataServiceBase(IAsyncRepository<TEntity> entityRepository)
+        {
+            Repository = entityRepository;
+        }
+
+        protected IAsyncRepository<TEntity> Repository { get; }
+    }
+}
