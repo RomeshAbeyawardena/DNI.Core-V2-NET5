c92c697743d854fec0a5a665cdf3f391c73b26b5
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-09 13:23:39 +0000

    - Added look up service

---
+++ DNI.Core.Shared/Contracts/IPagingCriteria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Shared.Contracts
+{
+    public interface IPagingCriteria
+    {
+        int TotalItemsPerPage { get; set; }
+        int PageIndex { get; set; }
+    }
+}
---
+++ DNI.Core.Shared/Contracts/Services/ILookupService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Shared.Contracts.Services
+{
+    public interface ILookupService<TEntity>
+    {
+        TEntity Lookup(ILookupParameters parameters);
+        Task<TEntity> LookupAsync(ILookupParameters parameters);
+    }
+}
