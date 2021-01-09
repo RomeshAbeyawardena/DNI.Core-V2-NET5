36fd5b1f25f393b196b4292f348792e884c1d25f
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-09 14:31:20 +0000

    Updated lookup service

---
+++ DNI.Core.Shared/Contracts/Services/ILookupService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Shared.Contracts.Services
+{
+    public interface ILookupService<TKey, TEntity, TLookupParameters> : ILookup<TKey, TEntity>
+        where TLookupParameters : ILookupParameters
+    {
+        TEntity Lookup(TLookupParameters parameters);
+        Task<TEntity> LookupAsync(TLookupParameters parameters, CancellationToken cancellationToken);
+    }
+}
