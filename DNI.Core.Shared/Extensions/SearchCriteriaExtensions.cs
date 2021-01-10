ab205a16c87fe1e499b5f23f58597988796a69db
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-10 21:53:00 +0000

    Added SearchCriteriaExtensions and tests

---
+++ DNI.Core.Shared/Extensions/SearchCriteriaExtensions.cs
@@ -0,0 +1,63 @@
+﻿using DNI.Core.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Shared.Extensions
+{
+    public static class SearchCriteriaExtensions
+    {
+        public static IEnumerable<Tuple<PropertyInfo, object>> GetSearchParameters<T>(this ISearchCriteria<T> searchCriteria)
+        {
+            var parameters = new List<Tuple<PropertyInfo, object>>();
+            var entityType = typeof(T);
+
+            var properties = entityType.GetProperties();
+
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(searchCriteria.Parameters);
+                if(value.IsDefault())
+                {
+                    continue;
+                }
+
+                parameters.Add(Tuple.Create(property, value));
+            }
+
+            return parameters.ToArray();
+        }
+
+        public static Expression<Func<T, bool>> GetExpression<T>(this ISearchCriteria<T> searchCriteria, 
+            IEnumerable<Tuple<PropertyInfo, object>> searchParameters = default)
+        {
+            var entityType = typeof(T);
+
+            var parameters = searchParameters ?? GetSearchParameters(searchCriteria);
+            
+            var parameterExpression = Expression.Parameter(entityType);
+            Expression expression = default;
+            foreach (var parameter in parameters)
+            {
+                var propertyInfo = parameter.Item1;
+                var value = parameter.Item2;
+                //var variableExpression = Expression.Variable(propertyInfo.PropertyType);
+                
+                var constantExpression = Expression.Constant(value);
+                var propertyOrFieldexpression = Expression.MakeMemberAccess(parameterExpression, propertyInfo);
+
+                var equalexpression = Expression.Equal(propertyOrFieldexpression, constantExpression);
+
+                expression = (expression == default)
+                    ? equalexpression
+                    : Expression.Or(expression, equalexpression);
+            }
+
+            return Expression.Lambda<Func<T, bool>>(expression, "s", new [] { parameterExpression });
+        }
+    }
+}
---
+++ DNI.Core.Tests/SearchCriteriaExtensionsTests.cs
@@ -0,0 +1,76 @@
+﻿using DNI.Core.Shared.Contracts;
+using DNI.Core.Shared.Extensions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Tests
+{
+    public class SearchCriteriaExtensionsTests
+    {
+        [Test]
+        public void GetSearchParameters()
+        {
+            var expression = new StudentViewModel { Parameters = new Student { Name = "John" } }.GetSearchParameters();
+
+            Assert.AreEqual(1, expression.Count());
+
+            expression = new StudentViewModel { Parameters = new Student { Id = 1, Name = "John" } }.GetSearchParameters();
+
+            Assert.AreEqual(2, expression.Count());
+        }
+
+        [Test] 
+        public void GetExpression()
+        {
+            var students = new List<Student>
+            {
+                new Student { Id = 1, Name = "John" },
+                new Student { Id = 2, Name = "Harris" }
+            };
+
+            var expression = new StudentViewModel { Parameters = new Student { Name = "John" } }.GetExpression();
+
+            var foundStudents = students.Where(expression.Compile());
+
+            Assert.AreEqual(1, foundStudents.Count());
+            Assert.Contains(students[0], foundStudents.ToArray());
+            expression = new StudentViewModel { Parameters = new Student {  Name = "Harris" } }.GetExpression();
+
+            foundStudents = students.Where(expression.Compile());
+            
+            Assert.AreEqual(1, foundStudents.Count());
+            Assert.Contains(students[1], foundStudents.ToArray());
+
+            expression = new StudentViewModel { Parameters = new Student { Id = 1,  Name = "Harris" } }.GetExpression();
+
+            foundStudents = students.Where(expression.Compile());
+            
+            Assert.AreEqual(2, foundStudents.Count());
+            Assert.Contains(students[0], foundStudents.ToArray());
+            Assert.Contains(students[1], foundStudents.ToArray());
+
+            expression = new StudentViewModel { Parameters = new Student { Id = 3,  Name = "Harrison" } }.GetExpression();
+
+            foundStudents = students.Where(expression.Compile());
+            
+            Assert.AreEqual(0, foundStudents.Count());
+        }
+
+        private class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        private class StudentViewModel : ISearchCriteria<Student>
+        {
+            public Student Parameters { get; set; }
+            public int TotalItemsPerPage { get; set; }
+            public int PageIndex { get; set; }
+        }
+    }
+}
