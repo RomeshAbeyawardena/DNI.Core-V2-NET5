9c06b536c795347fa5760de9572389d9a693ff34
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-11 16:27:08 +0000

    Added ApiControllerBase

---
+++ DNI.Core.Web/Abstractions/ApiControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Web.Abstractions
+{
+    [ApiController]
+    public class ApiControllerBase : ControllerBase
+    {
+        protected ApiControllerBase(IMediator mediator, IMapper mapper) : base(mediator, mapper)
+        {
+        }
+    }
+}
