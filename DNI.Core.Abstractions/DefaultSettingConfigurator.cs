e49fa0333044fcaa1d4d8b61dea2cbe7e829c8bb
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-12 19:43:00 +0000

    Removed unused usings

--- DNI.Core.Abstractions/DefaultSettingConfigurator.cs
+++ DNI.Core.Abstractions/DefaultSettingConfigurator.cs
@@ -4,10 +4,7 @@ using DNI.Core.Shared.Extensions;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DNI.Core.Abstractions
 {
