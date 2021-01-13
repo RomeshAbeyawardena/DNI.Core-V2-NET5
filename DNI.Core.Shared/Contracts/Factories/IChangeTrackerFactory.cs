b80dadeb97f151e23de5a1eaf135abc72f26d3af
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-13 22:55:50 +0000

    Added intellisense

--- DNI.Core.Shared/Contracts/Factories/IChangeTrackerFactory.cs
+++ DNI.Core.Shared/Contracts/Factories/IChangeTrackerFactory.cs
@@ -2,11 +2,44 @@
 
 namespace DNI.Core.Shared.Contracts.Factories
 {
+    /// <summary>
+    /// Represents a change tracker factory to resolve <see cref="IChangeTracker{T} instances" /> 
+    /// </summary>
     public interface IChangeTrackerFactory
     {
+        /// <summary>
+        /// Resolves a <see cref="IChangeTracker{T}"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>An instance of a <see cref="IChangeTracker{T}"/></returns>
         IChangeTracker<T> GetChangeTracker<T>();
+        /// <summary>
+        /// Determines whether changes exist between <paramref name="source"/> and  <paramref name="value"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyChanges"></param>
+        /// <returns></returns>
         bool HasChanges<T>(T source, T value, out IEnumerable<PropertyChange> propertyChanges);
+
+        /// <summary>
+        /// Commits changes between <paramref name="source"/> and <paramref name="value"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
         int MergeChanges<T>(T source, T value);
+        /// <summary>
+        /// Commits changes between <paramref name="source"/> and <paramref name="value"/>
+        /// using an existing <paramref name="propertyChanges"/> list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyChanges"></param>
+        /// <returns></returns>
         int MergeChanges<T>(T source, T value, IEnumerable<PropertyChange> propertyChanges);
     }
 }
--- DNI.Core.Shared/Contracts/Providers/ICacheProvider.cs
+++ DNI.Core.Shared/Contracts/Providers/ICacheProvider.cs
@@ -1,11 +1,22 @@
-﻿using DNI.Core.Shared.Enumerations;
+﻿using DNI.Core.Shared.Contracts.Services;
+using DNI.Core.Shared.Enumerations;
 using System;
 
 namespace DNI.Core.Shared.Contracts.Providers
 {
+    /// <summary>
+    /// Represents an ICacheProvider that exposes methods available to an instance of <see cref="ICacheService"/>
+    /// </summary>
     public interface ICacheProvider : IDisposable
     {
+        /// <summary>
+        /// Gets or sets the <see cref="CacheServiceType"/> this instance exposes
+        /// </summary>
         CacheServiceType CacheServiceType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="SerializerType"/> this instance uses to serialize and deserialize data
+        /// </summary>
         SerializerType SerializerType { get; set; }
     }
 }
