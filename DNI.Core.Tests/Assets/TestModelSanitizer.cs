8233c3f9e2915855ea6eb59e67770f279e813115
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-17 01:15:45 +0000

    Added test assets

---
+++ DNI.Core.Tests/Assets/TestModelSanitizer.cs
@@ -0,0 +1,18 @@
+﻿using DNI.Core.Abstractions;
+using DNI.Core.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Tests.Assets
+{
+    public class TestModelSanitizer : ModelSanitizerBase
+    {
+        public override string SanitizeString(string propertyValue)
+        {
+            return base.SanitizeString(propertyValue);
+        }
+    }
+}
