f92ad48aeebb1046a42524818e4329e86cc3c274
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-19 19:26:04 +0000

    Added GetPagedItemsAsync

---
+++ DNI.Core.Shared/Extensions/PagerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using DNI.Core.Shared.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Shared.Extensions
+{
+    public static class PagerExtensions
+    {
+        public static Task<IEnumerable<T>> GetPagedItemsAsync<T>(
+            IPager<T> pager, 
+            ISearchCriteria<T> searchCriteria, 
+            CancellationToken cancellationToken)
+        {
+            return pager.GetPagedItemsAsync(searchCriteria.PageIndex, 
+                searchCriteria.TotalItemsPerPage, cancellationToken);
+        }
+    }
+}
