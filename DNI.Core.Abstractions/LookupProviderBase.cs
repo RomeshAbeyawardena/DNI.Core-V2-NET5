ff165c2a88ddd7c6a4d1790171c284a65b01b976
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-24 22:38:13 +0000

    Added lookup provider base

---
+++ DNI.Core.Abstractions/LookupProviderBase.cs
@@ -0,0 +1,38 @@
+﻿using DNI.Core.Shared.Extensions;
+using DNI.Core.Shared.Contracts.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Abstractions
+{
+    public abstract class LookupProviderBase
+    {
+        public LookupProviderBase(IModelEncryptionFactory modelEncryptionFactory)
+        {
+            this.modelEncryptionFactory = modelEncryptionFactory;
+        }
+
+        protected T GetResult<T, TKey>(TKey value,
+            Expression<Func<T, TKey>> getProperty,
+            Func<TKey, T> getResult, params object[] args)
+        {
+            var property = getProperty.GetProperty();
+
+            var instance = (T)Activator.CreateInstance(typeof(T), args);
+
+            property.SetValue(instance, value);
+
+            modelEncryptionFactory.Decrypt(instance);
+
+            var val = getProperty.Compile().Invoke(instance);
+
+            return getResult(val);
+        }
+
+        private readonly IModelEncryptionFactory modelEncryptionFactory;
+    }
+}
