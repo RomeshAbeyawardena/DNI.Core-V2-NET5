0add974085b07c6486c29995bf8014566d7b3bc2
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-24 23:37:17 +0000

    Added repository factory

---
+++ DNI.Core.Abstractions/Factories/RepositoryFactory.cs
@@ -0,0 +1,31 @@
+﻿using DNI.Core.Shared.Contracts;
+using DNI.Core.Shared.Contracts.Factories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Abstractions.Factories
+{
+    internal class RepositoryFactory : IRepositoryFactory
+    {
+        public RepositoryFactory(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public IAsyncRepository<TEntity> GetAsyncRepository<TEntity>() where TEntity : class
+        {
+            return serviceProvider.GetRequiredService<IAsyncRepository<TEntity>>();
+        }
+
+        public IRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+        {
+            return serviceProvider.GetRequiredService<IRepository<TEntity>>();
+        }
+
+        private readonly IServiceProvider serviceProvider;
+    }
+}
---
+++ DNI.Core.Shared/Contracts/Factories/IRepositoryFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Shared.Contracts.Factories
+{
+    public interface IRepositoryFactory
+    {
+        IAsyncRepository<TEntity> GetAsyncRepository<TEntity>()
+            where TEntity : class;
+
+        IRepository<TEntity> GetRepository<TEntity>()
+            where TEntity : class;
+    }
+}
