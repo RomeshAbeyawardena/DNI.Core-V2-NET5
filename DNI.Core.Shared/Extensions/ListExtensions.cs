936e9431ee26136a0ac13d402e8c7fe5ab74a8fd
Anthony Romesh Abeyawardena <romesh.a@outlook.com>
2021-01-31 13:20:30 +0000

    Added list extensions

---
+++ DNI.Core.Shared/Extensions/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNI.Core.Shared.Extensions
+{
+    public static class ListExtensions
+    {
+        public static void Deallocate<T>(ref List<T> list,
+            GCCollectionMode collectionMode = GCCollectionMode.Forced,
+            bool blocking = false)
+        {
+            list.Clear();
+            list.TrimExcess();
+            list = null;
+            GC.Collect(
+                    GC.GetGeneration(list), 
+                    GCCollectionMode.Forced, 
+                    blocking);
+        }
+    }
+}
